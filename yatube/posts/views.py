56f1b3fcc7e9ecd309ce07f208f6b9f79b262ba4
Прылипко Егор <Egorprylipko@mail.ru>
2022-10-15 15:47:56 +0300

    Добавлены urls и view-функции и немного фиксов

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse('Слава пиву, алкашам слава')
+
+
+def group_posts(request, slug):
+    return HttpResponse('Ты не туда защел пацанчик это наш район')
