bc9389eaead1b3553194f710ad16186625ae8449
Прылипко Егор <Egorprylipko@mail.ru>
2022-10-17 20:12:52 +0300

    Добавил и настроил шаблоны

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('Слава пиву, алкашам слава')
+    template = 'posts/index.html'
+    return render(request, template)
 
 
 def group_posts(request, slug):
-    return HttpResponse('Ты не туда защел пацанчик это наш район')
+    template = 'posts/group_list.html'
+    return render(request, template)
